98da4363f1246af800180026cf1071c04e2545cd
gurb <pcgezer@hotmail.com>
2024-04-23 23:25:15 +0300

    ui changes for security

---
+++ BlazorUI/Models/Post/LoginVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorUI.Models.Post;
+
+public class LoginVM
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
