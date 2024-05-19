730d50405074fcb46a17b67dbaf450d9a82350ec
gurb <pcgezer@hotmail.com>
2024-05-19 11:44:35 +0300

    admin login

---
+++ AdminUI/Pages/Index.razor.cs
@@ -0,0 +1,56 @@
+﻿using AdminUI.Contracts;
+using AdminUI.Providers;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using AdminUI.Models.Section;
+using AdminUI.Services.UI;
+
+namespace AdminUI.Pages;
+
+public partial class Index
+{
+    [Inject]
+    private AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public IAuthenticationService AuthenticationService { get; set; }
+
+    [Inject]
+    public ISectionService SectionsService { get; set; }
+
+    [Inject]
+    public RefreshStateService RefreshStateService { get; set; }
+
+    protected async override Task OnInitializedAsync()
+    {
+        await ((ApiAuthenticationStateProvider)AuthenticationStateProvider).GetAuthenticationStateAsync();
+    }
+
+    protected void GoToLogin()
+    {
+        NavigationManager.NavigateTo("login/");
+    }
+
+    protected async Task AddSection(SectionVM section)
+    {
+        await SectionsService.CreateSection(section);
+        if(RefreshStateService.RefreshSectionList != null)
+        {
+            RefreshStateService.RefreshSectionList.Invoke();
+        }
+    }
+
+
+    protected void GoToRegister()
+    {
+        NavigationManager.NavigateTo("register/");
+    }
+
+    protected async void Logout()
+    {
+        await AuthenticationService.Logout();
+    }
+}
--- AdminUI/Program.cs
+++ AdminUI/Program.cs
@@ -1,11 +1,39 @@
 using AdminUI;
+using AdminUI.Contracts;
+using AdminUI.Handlers;
+using AdminUI.Providers;
+using AdminUI.Services.Base;
+using AdminUI.Services.Common;
+using AdminUI.Services.UI;
+using AdminUI.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddTransient<JwtAuthorizationMessageHandler>();
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7147")).AddHttpMessageHandler<JwtAuthorizationMessageHandler>();
+
+//builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<LocalStorageService>();
+builder.Services.AddScoped<RefreshStateService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IHeadingService, HeadingService>();
+builder.Services.AddScoped<ISectionService, SectionService>();
+builder.Services.AddScoped<ILikeService, LikeService>();
+builder.Services.AddScoped<IFavoriteService, FavoriteService>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
