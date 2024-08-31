5b7071237f46ad441e06926118b6c4677b38604b
gurb <pcgezer@hotmail.com>
2024-09-01 01:40:09 +0300

    stat design

---
+++ AdminUI/Pages/Charts/BrowserView.razor.js
@@ -0,0 +1,33 @@
+﻿export function drawBrowserViewChart() {
+    let ctx = document.getElementById('browserViewChart');
+
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+            ],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+}
--- AdminUI/Pages/Charts/TotalView.razor.js
+++ AdminUI/Pages/Charts/TotalView.razor.js
@@ -2,21 +2,28 @@
     let ctx = document.getElementById('totalViewChart');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
+            labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
+            datasets: [
+                {
+                    label: "Car A - Speed (mph)",
+                    data: [0, 59, 75, 20, 20, 55, 40],
+                    lineTension: 0,
+                    fill: true,
+                    borderColor: 'red'
+                },
+                {
+                    label: "Car B - Speed (mph)",
+                    data: [20, 15, 60, 60, 65, 30, 70],
+                    lineTension: 0,
+                    fill: true,
+                    borderColor: 'blue'
+                }
+            ]
         },
         options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+            legend: { display: false }
         }
     });
 }
