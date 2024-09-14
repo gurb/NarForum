0fd5f27fc92bcf1269adba27e7b491ba25dd600e
gurb <pcgezer@hotmail.com>
2024-09-14 09:58:15 +0300

    add media query component

---
+++ BlazorUI/Pages/Component/MediaQuery.razor.js
@@ -0,0 +1,25 @@
+﻿let mediaQuery;
+export function RegisterChangeCallback(dotnetHelper) {
+    window.addEventListener('resize', () => {
+        dotnetHelper.invokeMethodAsync('OnResize');
+    });
+}
+
+export function MinWidthPx(px) {
+    mediaQuery = window.matchMedia("(min-width:" + px + "px)");
+}
+
+export function CheckQuery() {
+    if (mediaQuery != null) {
+        return handleMediaQueryChange(mediaQuery);
+    }
+    return true;
+}
+
+function handleMediaQueryChange(e) {
+    if (e.matches) {
+        return true;
+    } else {
+        return false;
+    }
+}
