de60582935f39f82ec1cefafaf478ed07b4ce136
gurb <pcgezer@hotmail.com>
2024-09-26 01:13:34 +0300

    add category modal

--- BlazorUI/Models/Category/CategoryVM.cs
+++ BlazorUI/Models/Category/CategoryVM.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
 		public Guid? SectionId { get; set; }
         public Guid? ParentCategoryId { get; set; }
         public int HeadingCounter { get; set; }
