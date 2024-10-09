38ba2d61c62dc640792a7d5d45b356797a6c1411
gurb <pcgezer@hotmail.com>
2024-10-09 11:48:44 +0300

    remove isactive status category and its children

--- Application/Features/Category/Commands/RemoveCategory/RemoveCategoryCommandHandler.cs
+++ Application/Features/Category/Commands/RemoveCategory/RemoveCategoryCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Contracts.Persistence;
+using Application.Extensions.Core;
 using AutoMapper;
+using Domain;
 using MediatR;
 
 namespace Application.Features.Category.Commands.RemoveCategory
@@ -9,11 +11,15 @@ namespace Application.Features.Category.Commands.RemoveCategory
     {
         private readonly IMapper _mapper;
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IHeadingRepository _headingRepository;
+        private readonly IPostRepository _postRepository;
 
-        public RemoveCategoryCommandHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        public RemoveCategoryCommandHandler(IMapper mapper, ICategoryRepository categoryRepository, IHeadingRepository headingRepository, IPostRepository postRepository)
         {
             _mapper = mapper;
             _categoryRepository = categoryRepository;
+            _headingRepository = headingRepository;
+            _postRepository = postRepository;
         }
 
         public async Task<Guid> Handle(RemoveCategoryCommand request, CancellationToken cancellationToken)
@@ -31,6 +37,7 @@ namespace Application.Features.Category.Commands.RemoveCategory
                 {
                     category.IsActive = !category.IsActive;
                     await _categoryRepository.UpdateAsync(category);
+                    await UpdateCategoryIsActive(category);
                     return category.Id;
                 }
             }
@@ -39,6 +46,49 @@ namespace Application.Features.Category.Commands.RemoveCategory
                 var message = ex.Message;
             }
             return Guid.Empty;
+        }
+
+        private async Task UpdateCategoryIsActive (Domain.Category category)
+        {
+            var predicate = PredicateBuilder.True<Domain.Category>();
+
+            predicate = predicate.And(x => x.ParentCategoryId == category.Id);
+
+            var childrenOfCategory = await _categoryRepository.GetAllAsync(predicate, true);
+
+            foreach (var child in childrenOfCategory) 
+            {
+                child.IsActive = category.IsActive;
+                await UpdateCategoryIsActive(child);    
+            }
+            _categoryRepository.UpdateList(childrenOfCategory);
+
+            var predicateHeading = PredicateBuilder.True<Domain.Heading>();
+
+            predicateHeading = predicateHeading.And(x => x.CategoryId == category.Id);
+
+            var headingsOfCategory = await _headingRepository.GetAllAsync(predicateHeading, true);
+
+            foreach (var child in headingsOfCategory)
+            {
+                child.IsActive = category.IsActive;
+                await UpdateHeadingIsActive(child);
+            }
+            _headingRepository.UpdateList(headingsOfCategory);
+        }
+
+        private async Task UpdateHeadingIsActive (Domain.Heading heading)
+        {
+            var predicatePost = PredicateBuilder.True<Domain.Post>();
+
+            predicatePost = predicatePost.And(x => x.HeadingId == heading.Id);
+
+            var postsOfHeading = await _postRepository.GetAllAsync(predicatePost, true);
+            foreach (var child in postsOfHeading)
+            {
+                child.IsActive = heading.IsActive;
+            }
+            _postRepository.UpdateList(postsOfHeading);
         }
     }
 }
