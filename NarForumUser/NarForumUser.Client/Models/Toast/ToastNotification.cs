892b6a9c0e3bffe7170b342ad8cc1c1b5bd8a3ef
gurb <pcgezer@hotmail.com>
2024-10-13 00:29:21 +0300

    add toast notification

--- NarForumUser/NarForumUser.Client/Models/Toast/ToastNotification.cs
+++ NarForumUser/NarForumUser.Client/Models/Toast/ToastNotification.cs
@@ -4,9 +4,11 @@ namespace NarForumUser.Client.Models.Toast
 {
     public class ToastNotification
     {
-        public ToastTypeVM Type { get; set; } 
+        public ToastTypeVM Type { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
         public string? Title { get; set; }
         public string? RightText { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
         public string? Message { get; set; }
         public bool CanHide { get; set; }
         public bool IsShow { get; set; }
